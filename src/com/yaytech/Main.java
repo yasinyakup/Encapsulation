b669a8c1736753fb1f176e7a56c7d25d54db4813
yasin yakup <yasinyakup@gmail.com>
2020-12-04 14:19:44 +0300

    getter and setter

--- src/com/yaytech/Main.java
+++ src/com/yaytech/Main.java
@@ -1,7 +1,11 @@
 package com.yaytech;
 
+import com.test.Student;
+
 public class Main {
+    int a;
     public static void main(String[] args) {
         System.out.println("This is an encapsulation test!");
+        St
     }
 }
